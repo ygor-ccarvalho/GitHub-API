53a05f54503b61b38d594f2bfcb4f09ab7a8b7cf
Ygor-CCarvalho <ygorcdc@gmail.com>
2024-01-22 20:30:58 -0300

    Código revisado separando serviços, variáveis e objetos em arquivos para facilitar a leitura e manutenção

---
+++ src/scripts/index.js
@@ -0,0 +1,34 @@
+import { getUser } from "./services/user.js"
+import { getRepositories } from "./services/repositories.js"
+
+import { user } from "./objects/user.js"
+import { screen } from "./objects/screen.js"
+
+
+document.getElementById('btn-search').addEventListener('click', () => {
+    const userName = document.getElementById('input-search').value    
+    getUserData(userName)
+})
+
+document.getElementById('input-search').addEventListener('keyup',(e) =>{
+    const userName = e.target.value
+    const key = e.wich || e.keyCode
+    const isEnterKeyPressed = key === 13
+
+    if(isEnterKeyPressed){
+        getUserData(userName)
+    }
+})
+
+async function getUserData(userName){
+
+    const userResponse = await getUser(userName)
+    const repositoriesResponse = await getRepositories(userName)
+
+    user.setInfo(userResponse)
+    user.setRepositories(repositoriesResponse)
+
+    console.log(user)
+    screen.renderUser(user)
+
+}
